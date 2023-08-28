e72ecd8b5093793566025d058ead02a897ec6eb7
Mag024 <141672938+Mag024@users.noreply.github.com>
2023-08-28 11:49:30 -0700

    Add files via upload

---
+++ forAndWhileLoop082823.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 50; i++){
+        cout << "\n\n i is " << i;
+
+        cout << "\n\n" << endl;
+    }
+
+    // string index with increment
+    string myStr;
+    myStr = "abcdefghij";
+    cout << myStr[0] << endl;
+    cout << myStr[1] << endl;
+    cout << myStr[2] << endl;
+
+    cout << "\n\n" << endl;
+
+
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << myStr[i];
+        cout << "\n\n";
+    }
+
+
+    // decrement of alphabet
+    for (int i = 9; i > 0; i--)
+    {
+        cout << myStr[i];
+         cout << "\n\n";
+    }
+
+    // chatGPT solution
+    for (char c = 'a'; c <= 'j'; ++c)
+    {
+        cout << c;
+        cout << "\n\n\n";
+    }
+
+    // ChatGOT opposite, decrement
+      for (char c = 'j'; c >= 'a'; --c)
+    {
+        cout << c;
+        cout << "\n\n\n";
+    }
+
+    //1D Char array and fill it with chars a-h 1104
+    char myChars[10] = {'a','b','c','d','e','f','g','h','i','j'};
+
+    cout << "\n\n The letter should show e..." << myChars[4] << "\n\n";
+
+    // hexidimal garbage according to ChaptGPT, Angelo looked up
+    // &myChars would show Hexidicaml address
+    cout << myChars << "\n\n";
+
+    for (int i = 0; i < 10; i++){
+        cout << "\n\n myChars[" << i << "] is... " << myChars[i] << "\n\n";
+    }
+
+    //If statement 1130
+    int num1 = 33;
+    int num2 = 111;
+    if (num1 > num2){
+       cout << "\n\n num1 is the larger value, this expression is true";
+    }
+    else {
+        cout << "\n\n this statement is false";
+    }
+
+
+    return 0;
+}
